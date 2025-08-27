41687533735cc5e209c716885c0067855d5a48f0
ledwastaken <ledwastaken@proton.me>
2025-08-27 08:43:31 +0200

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            [
+              cmake
+              pkg-config
+              SDL2
+              vulkan-headers
+              vulkan-loader
+              vulkan-validation-layers
+              vulkan-memory-allocator
+              shaderc
+              spirv-tools
+              gcc
+              gdb
+            ];
+          shellHook = ''
+            export LSAN_OPTIONS=suppressions=$PWD/suppr.txt
+          '';
+        };
+      }
+    );
+}
