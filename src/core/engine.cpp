1eaa7dd1e4a42323fd403abcd1784ebfef6ddbd2
ledwastaken <ledwastaken@proton.me>
2025-09-22 10:00:20 +0200

    feat(engine): add create_device method

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -13,6 +13,7 @@ namespace core
   {
     create_window();
     create_instance();
+    create_device();
   }
 
   void Engine::loop()
@@ -31,6 +32,7 @@ namespace core
 
   void Engine::quit()
   {
+    vkDestroyDevice(device_, nullptr);
     vkDestroyInstance(instance_, nullptr);
 
     SDL_DestroyWindow(window_);
@@ -79,5 +81,118 @@ namespace core
     // Create the instance.
     if (vkCreateInstance(&createInfo, nullptr, &instance_) != VK_SUCCESS)
       throw std::runtime_error("failed to create vulkan instance");
+  }
+
+  void Engine::create_device()
+  {
+    // First figure out how many devices are in the system.
+    uint32_t physical_device_count = 0;
+    VkResult result = vkEnumeratePhysicalDevices(instance_, &physical_device_count, nullptr);
+    if (result != VK_SUCCESS)
+      throw std::runtime_error("failed to enumerate physical devices");
+
+    if (physical_device_count == 0)
+      throw std::runtime_error("no physical device available");
+
+    auto physical_devices = std::vector<VkPhysicalDevice>(physical_device_count);
+    result = vkEnumeratePhysicalDevices(instance_, &physical_device_count, physical_devices.data());
+    if (result != VK_SUCCESS)
+      throw std::runtime_error("failed to enumerate physical devices");
+
+    choose_physical_device(physical_devices);
+
+    // FIXME
+    VkPhysicalDeviceFeatures supported_features;
+    VkPhysicalDeviceFeatures required_features = {};
+
+    vkGetPhysicalDeviceFeatures(physical_device_, &supported_features);
+
+    required_features.multiDrawIndirect = supported_features.multiDrawIndirect;
+    required_features.tessellationShader = VK_TRUE;
+    required_features.geometryShader = VK_TRUE;
+
+    const char* const swapchain_extension[] = {
+      "VK_KHR_swapchain",
+    };
+
+    const VkDeviceQueueCreateInfo queue_create_info = {
+      .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+      .pNext = nullptr,
+      .flags = 0,
+      .queueFamilyIndex = 0,
+      .queueCount = 1,
+      .pQueuePriorities = nullptr,
+    };
+
+    const VkDeviceCreateInfo deviceCreateInfo = {
+      .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+      .pNext = nullptr,
+      .flags = 0,
+      .queueCreateInfoCount = 1,
+      .pQueueCreateInfos = &queue_create_info,
+      .enabledLayerCount = 0,
+      .ppEnabledLayerNames = nullptr,
+      .enabledExtensionCount = 1,
+      .ppEnabledExtensionNames = swapchain_extension,
+      .pEnabledFeatures = &required_features,
+    };
+
+    result = vkCreateDevice(physical_device_, &deviceCreateInfo, nullptr, &device_);
+    if (result != VK_SUCCESS)
+      throw std::runtime_error("failed to create logical device");
+  }
+
+  void Engine::choose_physical_device(std::vector<VkPhysicalDevice> devices)
+  {
+    int max_score = -1;
+
+    for (int i = 0; i < devices.size(); i++)
+    {
+      VkPhysicalDeviceProperties properties;
+      VkPhysicalDeviceFeatures features;
+      VkPhysicalDeviceMemoryProperties memory_properties;
+
+      vkGetPhysicalDeviceProperties(devices[i], &properties);
+      vkGetPhysicalDeviceFeatures(devices[i], &features);
+      vkGetPhysicalDeviceMemoryProperties(devices[i], &memory_properties);
+
+      if (required_features_not_supported(properties, features, memory_properties))
+        continue;
+
+      // int graphics_compute_family = -1;
+      // int present_family = -1;
+
+      // uint32_t family_count;
+      // vkGetPhysicalDeviceQueueFamilyProperties(devices[i], &family_count, NULL);
+
+      // auto family_properties = new VkQueueFamilyProperties[family_count];
+      // vkGetPhysicalDeviceQueueFamilyProperties(devices[i], &family_count, family_properties);
+
+      int score = calculate_device_score(properties, features, memory_properties);
+      if (score > max_score)
+      {
+        physical_device_ = devices[i];
+        max_score = score;
+      }
+    }
+
+    if (max_score < 0)
+      throw std::runtime_error("no physical device fits minimum requirements");
+  }
+
+  bool Engine::required_features_not_supported(VkPhysicalDeviceProperties properties,
+                                               VkPhysicalDeviceFeatures features,
+                                               VkPhysicalDeviceMemoryProperties memory_properties)
+  {
+    // FIXME
+    return false;
+  }
+
+  int Engine::calculate_device_score(VkPhysicalDeviceProperties properties,
+                                     VkPhysicalDeviceFeatures features,
+                                     VkPhysicalDeviceMemoryProperties memory_properties)
+  {
+    // FIXME
+    return 0;
   }
 } // namespace core
--- src/core/engine.h
+++ src/core/engine.h
@@ -27,8 +27,18 @@ namespace core
   private:
     void create_window();
     void create_instance();
+    void create_device();
+    void choose_physical_device(std::vector<VkPhysicalDevice> physical_devices);
+    bool required_features_not_supported(VkPhysicalDeviceProperties properties,
+                                         VkPhysicalDeviceFeatures features,
+                                         VkPhysicalDeviceMemoryProperties memory_properties);
+    int calculate_device_score(VkPhysicalDeviceProperties properties,
+                               VkPhysicalDeviceFeatures features,
+                               VkPhysicalDeviceMemoryProperties memory_properties);
 
     SDL_Window* window_;
     VkInstance instance_ = VK_NULL_HANDLE;
+    VkPhysicalDevice physical_device_ = VK_NULL_HANDLE;
+    VkDevice device_ = VK_NULL_HANDLE;
   };
 } // namespace core
