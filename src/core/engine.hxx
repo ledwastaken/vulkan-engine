9bdb21d12028a698ebb5b8fc1f94c55351267a59
ledwastaken <ledwastaken@proton.me>
2025-09-29 09:17:02 +0200

    feat(engine): add get_command_buffer method

--- src/core/engine.hxx
+++ src/core/engine.hxx
@@ -2,5 +2,10 @@
 
 namespace core
 {
+  inline VkCommandBuffer Engine::get_command_buffer(uint32_t image_index) const
+  {
+    return graphics_command_buffers_[image_index];
+  }
+
   inline VkDevice Engine::get_device() const { return device_; }
 } // namespace core
