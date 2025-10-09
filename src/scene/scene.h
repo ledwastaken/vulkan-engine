a1b75042daf0cae41feffdf49e929ff25e858610
ledwastaken <ledwastaken@proton.me>
2025-10-09 15:43:43 +0200

    feat(scene): add skybox data attributes

--- src/scene/scene.h
+++ src/scene/scene.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vulkan/vulkan.h>
+
 #include "scene/camera.h"
 #include "scene/instance.h"
 #include "scene/visitor.h"
@@ -15,7 +17,16 @@ namespace scene
 
     void accept(Visitor& visitor) override;
 
+    VkImage get_skybox_image() const;
+    VkImageView get_skybox_image_view() const;
+    VkSampler get_skybox_sampler() const;
+
     Camera* current_camera = nullptr;
+
+  private:
+    VkImage skybox_image_ = VK_NULL_HANDLE;
+    VkImageView skybox_image_view_ = VK_NULL_HANDLE;
+    VkSampler skybox_sampler_ = VK_NULL_HANDLE;
   };
 } // namespace scene
 
--- src/scene/scene.hxx
+++ src/scene/scene.hxx
@@ -3,4 +3,7 @@
 namespace scene
 {
   inline void Scene::accept(Visitor& visitor) { visitor(*this); }
+  inline VkImage Scene::get_skybox_image() const { return skybox_image_; }
+  inline VkImageView Scene::get_skybox_image_view() const { return skybox_image_view_; }
+  inline VkSampler Scene::get_skybox_sampler() const { return skybox_sampler_; }
 } // namespace scene
